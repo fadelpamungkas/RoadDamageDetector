47cded03301d7bc817a024ee86af9d345c9e8eac
Fadel <fadelpamungkas3@gmail.com>
2021-05-13 02:10:47 +0700

    - Update BottomNavigationView - Implement NavigationComponent - Add HomeFragment and ProfileFragment - CameraX

--- app/src/main/java/com/example/roaddamagedetector/MainActivity.kt
+++ app/src/main/java/com/example/roaddamagedetector/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.roaddamagedetector
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.roaddamagedetector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_camera, R.id.navigation_profile
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
 }
